69d9b426077707686d27e3b48e03e905dfc31a8b
Krishan <krishan142536@gmail.com>
2024-03-19 20:05:42 +0530

    in

---
+++ moh/src/SideBar.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+
+const SideBar = () => {
+  useEffect(() => {
+    const links = document.querySelectorAll('.nav-link');
+    const currentUrl = window.location.pathname;
+
+    links.forEach(link => {
+      if (link.getAttribute('href') === currentUrl) {
+        link.classList.add('clicked');
+      }
+
+      link.addEventListener('click', function() {
+        links.forEach(otherLink => {
+          otherLink.classList.remove('clicked');
+        });
+
+        this.classList.add('clicked');
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div className="position-sticky pt-3 sidebar-sticky">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/home">
+                <b><i className="bi bi-people-fill"></i> Home</b>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/">
+                <b><i className="bi bi-people-fill"></i> Newly Married Couple &nbsp;&nbsp;&nbsp; Management</b>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/index2">
+                <b><i className="bi bi-gender-female"></i> Pregnant Mothers Management</b>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">
+                <b><i className="bi bi-file-earmark-person"></i> Baby's Information and Vaccination Management</b>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default SideBar;
