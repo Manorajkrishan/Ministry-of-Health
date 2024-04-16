f7d5a1ed25fa2e3b60ea4d95e5d65197e3d09cb9
IT22026620 <it22026620@my.sliit.lk>
2024-04-16 11:04:24 +0530

    Create motherdetails.js in models

---
+++ mother/backend/models/motherdetails.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MotherSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+
+    age:{
+        type:Number,
+        required:true
+    },
+
+    bloodgroup:{
+        type:String,
+        required:true
+    },
+
+    pregnantmonthcount:{
+        type:Number,
+        required:true
+    },
+
+    contact:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    address:{
+        type:String,
+        required:true
+    },
+
+    lastconsult:{
+        type:Date,
+        required:true
+    },
+
+    nextconsult:{
+        type:Date,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    status:{
+        type:String
+    }
+
+});
+
+module.exports = mongoose.model('Mother', MotherSchema);
+//module.exports = mongoose.model('Mothers', MotherSchema);
