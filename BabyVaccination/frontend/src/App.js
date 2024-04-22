db92e89c6a2d403f74d2a4f8088186977646f608
IT22049490 <it22049490@my.sliit.lk>
2024-04-22 19:34:15 +0530

    Update App.js

--- BabyVaccination/frontend/src/App.js
+++ BabyVaccination/frontend/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Header from './Component/Header';
+import Sidebar from './Component/Sidebar';
+import Allbabies from './Component/Allbabies';
+import AddBabies from './Component/Addbabies';
+import ViewBaby from './Component/Viewbaby';
+import EditBaby from './Component/updatebaby';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <div className="row">
+          {/* Sidebar */}
+         
+          <div className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div className="position-sticky pt-3 sidebar-sticky">
+              
+            </div>
+          </div>
+          {/* Header */}
+          <Header/>
+          {/* Main Content */}
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <Routes>
+              <Route path="/allbaby" element={<Allbabies />} />
+              <Route path="/addbaby" element={<AddBabies />} />
+              <Route path="/viewbaby/:id" element={<ViewBaby/>}/>
+              <Route path="/editbaby/:id" element={<EditBaby/>}/>
+         
+           
+             
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
 }
 
